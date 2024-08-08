f0dc9d420a6741398817dadd0a0fb9bc888dda41
princepali <princepali536@gmail.com>
2024-08-08 21:07:32 +0530

    Add project files.

---
+++ Prince Mobile Collection/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Prince_Mobile_Collection.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Prince Mobile Collection/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Prince_Mobile_Collection.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
